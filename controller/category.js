102b1f5ab08f545981360806e19c3651a5725a67
XxPlayerxX <ramankhadgi0@gmail.com>
2021-04-13 14:46:33 +0545

    fourth commit

--- controller/category.js
+++ controller/category.js
@@ -1,27 +1,56 @@
 
-const Category=require('../model/categoryModel')
+const Category = require('../model/categoryModel')
 
-exports.getHello=(req,res)=>{
+exports.getHello = (req, res) => {
     res.send('Hello World')
 }
 
-exports.postCategory=(req,res)=>{
+exports.postCategory = (req, res) => {
     //agadi ko request paxadi ko response
-    let category=new Category(req.body)
-    category.save((error,category)=>{
-        if(error || !category){
-            return res.status(400).json({error:"Something went wrong"})
+    let category = new Category(req.body)
+    category.save((error, category) => {
+        if (error || !category) {
+            return res.status(400).json({ error: "Something went wrong" })
         }
-        res.json({category})
+        res.json({ category })
     })
 }
 
 
-exports.getAllCategory=(req,res)=>{
-    Category.find().exec((error,category)=>{
-        if(error || !category){
-            return res.status(400).json({error:"Category not found"})
+exports.getAllCategory = (req, res) => {
+    Category.find().exec((error, category) => {
+        if (error || !category) {
+            return res.status(400).json({ error: "Category not found" })
         }
         res.json(category)
+    })
+}
+
+
+//category by id
+exports.CategoryById = (req, res, next, id) => {
+    Category.findById(id).exec((error, category) => {
+        if (error || !category) {
+            return res.status(400).json({ error: "Category not found" })
+        }
+        req.category = category
+        next()
+    })
+
+}
+
+exports.getsingleCategory = (req, res) => {
+    return res.json(req.category)
+}
+
+
+//to delete category
+exports.deleteCategory=(req,res)=>{
+    const category=req.category
+    category.remove((error,result)=>{
+        if(error || !result){
+            return res.status(400).json({error:"Something went wrong"})
+        }
+        res.json({messsage:"Category deleted"})
     })
 }
--- route/CategoryRoute.js
+++ route/CategoryRoute.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const { getHello, postCategory, getAllCategory } = require('../controller/category')
+const { getHello, postCategory, getAllCategory, CategoryById, getsingleCategory, deleteCategory } = require('../controller/category')
 const router = express.Router()
 
 router.get('/hello', getHello)
-router.post('/postcategory',postCategory)
-router.get('/categorylist',getAllCategory)
+router.post('/postcategory', postCategory)
+router.get('/categorylist', getAllCategory)
+router.param('categoryId', CategoryById)
+router.get('/read/:categoryId', getsingleCategory)
+router.delete('/deletecategory/:categoryId', deleteCategory)
 
-module.exports=router
+module.exports = router
